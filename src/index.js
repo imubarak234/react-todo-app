693d3c2d51a048cdc78d376552cd0a15b48dcb54
Mubarak <imubarak2424@gmail.com>
2022-01-10 11:19:48 +0100

    intial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import TodoContainer from "./components/TodoContainer.js"
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode> , document.getElementById("root")
+);
